9399e67ccce9fdaf55f40bf88ba07265ba5f8b12
baycao-cmd <dchnh0508@gmail.com>
2026-05-09 01:59:48 +0700

    Sua xong cai phan upload

--- backend/src/main/java/com/testcase/backend/entity/Relationship.java
+++ backend/src/main/java/com/testcase/backend/entity/Relationship.java
@@ -1,5 +1,7 @@
 package com.testcase.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.*;
--- backend/src/main/java/com/testcase/backend/entity/UseCaseDiagram.java
+++ backend/src/main/java/com/testcase/backend/entity/UseCaseDiagram.java
@@ -1,6 +1,8 @@
 package com.testcase.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
